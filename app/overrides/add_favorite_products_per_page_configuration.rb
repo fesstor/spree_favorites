7cfb804214adde95f0e46ec476c71ee08382899a
fesstor <emelya1993@rambler.ru>
2015-03-10 19:10:17 +0300

    Migrate to 3.0.0

--- app/overrides/add_favorite_products_per_page_configuration.rb
+++ app/overrides/add_favorite_products_per_page_configuration.rb
@@ -1,16 +1,20 @@
 Deface::Override.new(
   :virtual_path => 'spree/admin/general_settings/edit',
   :name => 'add_favorite_products_per_page_configuration',
-  :insert_after => "#preferences .row",
+  :insert_after => "#preferences .row:last-of-type",
   :text => %Q{
-    <div class="row">
-      <fieldset class="no-border-bottom">
-        <legend align="center"><%= Spree.t(:favorites_settings) %></legend>
-        <div class="field">
-          <%= label_tag :favorites_per_page, Spree.t(:favorites_per_page) %><br>
-          <%= text_field_tag :favorites_per_page, Spree::Config[:favorites_per_page], :size => 3 %>
+      <fieldset class="general no-border-top">
+        <div class="row">
+          <h3><%= Spree.t(:favorites_settings) %></h2>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group" data-hook="admin_favorites_setting_favorite">
+              <%= label_tag :favorites_per_page, Spree.t(:favorites_per_page) %><br>
+              <%= text_field_tag :favorites_per_page, Spree::Config[:favorites_per_page], class: 'form-control' %>
+            </div>
+          </div>
         </div>
       </fieldset>
-    </div>
   }
 )
